c931c30eac4b9b31cb4398f0c68acae1f9974ce2
Azadova Indira <aindira2197@gmail.com>
2026-03-10 14:51:34 +0500

    Restore BankCard class implementation

---
+++ main.py
@@ -0,0 +1,19 @@
+class BankCard:
+    bank_name = "Agrobank"
+
+    def __init__(self, card_number, balance):
+        self.card_number = card_number
+        self.balance = balance
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            return self.balance
+
+    @classmethod
+    def change_bank_name(cls, new_name):
+        cls.bank_name = new_name
+
+    @staticmethod
+    def is_valid_card_number(number):
+        if len(number) == 16:
